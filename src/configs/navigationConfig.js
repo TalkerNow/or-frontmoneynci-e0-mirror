6051b2d9bdda337f1495a0f3805eb6cee354ff51
enzo nivert <enzo@eor.fr>
2021-10-12 14:30:05 +0200

    right for dashboard page modified

--- src/configs/navigationConfig.js
+++ src/configs/navigationConfig.js
@@ -8,7 +8,7 @@ const navigationConfig = [
     title: "Dashboard",
     type: "item",
     icon: <Icon.Home size={20}/>,
-    permissions: ["admin", "Consultants"],
+    permissions: ["admin"],
     navLink: "/"
     //badge: "warning",
     //badgeText: "2"
