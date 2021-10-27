904e213f514b6b5b9427b81cd4a943cfbb0bc400
Erwann <shark90.leletty@gmail.com>
2021-10-27 12:11:30 +0200

    Pixel

--- src/views/apps/profile/index.js
+++ src/views/apps/profile/index.js
@@ -195,7 +195,7 @@ class UserView extends React.Component {
             </Card>
           </Col>
           <Col sm="12" md="6">
-            <Card style={{minHeight:'350px'}}>
+            <Card style={{minHeight:'360px'}}>
               <CardHeader>
                 <CardTitle><MessageCircle className="warning" size={25} /> Notes</CardTitle>
               </CardHeader>
