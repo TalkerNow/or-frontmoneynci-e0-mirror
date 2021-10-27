efe68b1fabaeb365b5dc176de5f5cafa4e690875
Erwann <shark90.leletty@gmail.com>
2021-10-27 11:31:55 +0200

    Spaces

--- src/views/apps/user/edit/Informations.js
+++ src/views/apps/user/edit/Informations.js
@@ -697,9 +697,6 @@ class UserAccountTab extends React.Component {
                     </FormGroup>
                 </Col>
                 <Col md="6" sm="12">
-
-                </Col>
-                <Col md="6" sm="12">
                     <FormGroup>
                         <Label for="child_nbr">Nom Société</Label>
                         <Input
