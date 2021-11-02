a61c4132810a59b03772d51cbbf25abbadbdaa81
Erwann <shark90.leletty@gmail.com>
2021-11-02 16:28:56 +0100

    test

--- src/views/pages/contract-template/CreateContract.js
+++ src/views/pages/contract-template/CreateContract.js
@@ -178,6 +178,11 @@ class CreateContract extends React.Component {
               user_id: this.props.match.params.id
           }
       );
+      this.setState(
+        {
+          parent_id: response.data.data.parent_id
+        }
+    );
       axios.get(global.config.server_url + "/get_template/1", Config).then(response => {
           if(response.data != null) {
               let values = JSON.parse(response.data.values);
