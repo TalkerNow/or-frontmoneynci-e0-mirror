8b403f067389bdce88d678f60672c079992bb965
Erwann <shark90.leletty@gmail.com>
2021-11-03 11:01:19 +0100

    No toString()

--- src/views/pages/contract-template/CreateContract.js
+++ src/views/pages/contract-template/CreateContract.js
@@ -220,8 +220,9 @@ class CreateContract extends React.Component {
       parameters['comment'] = "Contract de " + this.state.perso['first_name'] + " "+ this.state.perso['last_name'];
       parameters['advanced_payment'] = this.state.formValues['TOTALTTC']?this.state.formValues['TOTALTTC'] : 0;
       parameters['user_id'] = userid;
-      parameters['parent_id'] = parentid.toString(); //localStorage.getItem("parent_id"); //localStorage.getItem("userid");
+      parameters['parent_id'] = localStorage.getItem("parent_id"); //localStorage.getItem("userid");
       parameters['values'] = JSON.stringify(input_values);
+      console.log(this.props.match.params.parent_id);
         //-------- save Contract ---------
       axios.post(global.config.server_url + "/documents", parameters, Config)
           .then(function(result) {
