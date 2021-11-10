b8d82b4bb459f80b682d38a6b4b9ef9e245ef24a
enzo <enzo.nivert@epitech.eu>
2021-11-10 16:47:26 +0100

    menutheme success

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -3,7 +3,7 @@
 //Template config options
 const themeConfig = {
   layout: "vertical", // options[String]: "vertical"(default), "horizontal"
-  theme: "semi-dark", // options[String]: 'light'(default), 'dark', 'semi-dark'
+  theme: "light", // options[String]: 'light'(default), 'dark', 'semi-dark'
   sidebarCollapsed: false, // options[Boolean]: true, false(default)
   navbarColor: "default", // options[String]: default / primary / success / danger / info / warning / dark
   navbarType: "floating", // options[String]: floating(default) / static / sticky / hidden
@@ -11,7 +11,7 @@ const themeConfig = {
   disableCustomizer: true, // options[Boolean]: true, false(default)
   hideScrollToTop: false, // options[Boolean]: true, false(default)
   disableThemeTour: false, // options[Boolean]: true, false(default)
-  menuTheme: "primary", // options[String]: primary / success / danger / info / warning / dark
+  menuTheme: "success", // options[String]: primary / success / danger / info / warning / dark
   direction: "ltr" // options[String] : ltr / rtl
 };
 
