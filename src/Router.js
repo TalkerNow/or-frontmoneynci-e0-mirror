baee72fa0285a501aa776c87bdc627fd73dfdb6f
enzo <enzo.nivert@epitech.eu>
2021-11-10 10:56:28 +0100

    includes not include

--- src/Router.js
+++ src/Router.js
@@ -50,8 +50,7 @@ const ProtectedRoute = ({ component: Component, fullLayout, isAuth, authorisatio
   <Route
     {...rest}
     render={props => {
-      console.log(authorisation)
-      if (authorisation.include(isAuth.user, 0)) return (
+      if (authorisation.includes(isAuth.user, 0)) return (
         <ContextLayout.Consumer>
           {context => {
             let LayoutTag =
