fcdc7867c002067e1a0b774d55af2e3e0629f5a7
Erwann <shark90.leletty@gmail.com>
2021-12-14 11:40:53 +0100

    Capability to send email via clik on email

--- src/views/apps/user/list/OldClientsList.js
+++ src/views/apps/user/list/OldClientsList.js
@@ -74,16 +74,6 @@ class OldClientsList extends React.Component {
         field: "cl_nom",
         filter: true,
         width: 250,
-        /*cellRendererFramework: rowData => {
-          return (
-            <div
-              className="d-flex align-items-center cursor-pointer"
-              onClick={() => history.push("/app/user/oldedit/" + rowData.data.clcleunik + "/1", rowData.data.clcleunik)}
-            >
-              <span>{rowData.data.cl_nom}</span>
-            </div>
-          )
-        }*/
       },
       {
         headerName: "Prénom",
@@ -109,26 +99,32 @@ class OldClientsList extends React.Component {
         filter: true,
         width: 250,
       },
-      /*{
-        headerName: "Technicien Nom",
-        field: "technician_name",
+      {
+        headerName: "Ville",
+        field: "cl_ville",
         filter: true,
         width: 250,
-        cellRendererFramework: params => {
-          return (
-              <div
-                  className="d-flex align-items-center cursor-pointer"
-              >
-                <span>{params.data.parent? params.data.parent.name:""}</span>
-              </div>
-          )
-        }
-      },*/
+      },
+      {
+        headerName: "Code postal",
+        field: "cl_cp",
+        filter: true,
+        width: 250,
+      },
       {
         headerName: "Email",
         field: "cl_mail",
         filter: true,
-        width: 200
+        width: 200,
+        cellRendererFramework: rowData => {
+          var email = rowData.data.cl_mail;
+          return (
+            <div
+              className="d-flex align-items-center cursor-pointer"
+              onClick={() => window.location.href = "mailto:"+email+"?subject=Subject&body=message%20goes%20here"}
+            ><span>{rowData.data.cl_mail}</span></div>
+          )
+        }
       },
       {
         headerName: "Date de Création",
