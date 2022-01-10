869a8d1dfe36831e2be15153e4f2ca59e8494c7d
enzo <enzo.nivert@epitech.eu>
2022-01-10 16:59:07 +0100

    small correction on revenue graph and Expert Module

--- src/views/apps/dashboard/ExpertCard.js
+++ src/views/apps/dashboard/ExpertCard.js
@@ -1,18 +1,25 @@
 import React from "react"
 import { Users } from "react-feather"
 import axios from "axios";
-
-
-import {Card,
-  CardBody,
+import { ContextLayout } from "../../../utility/context/Layout"
+import { AgGridReact } from "ag-grid-react"
+import {
   Nav,
   NavItem,
   NavLink,
   TabContent,
+  TabPane,
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
   Input,
-  CardHeader, CardTitle,
-  TabPane} from "reactstrap";
-  import classnames from "classnames"
+  Row,
+  Col,
+} from "reactstrap"
+import classnames from "classnames"
+import "../../../assets/scss/plugins/tables/_agGridStyleOverride.scss"
+import "../../../assets/scss/pages/users.scss"
 
 const Config = {
   headers: {
@@ -24,8 +31,51 @@ const FrenchMonth = ['janvier', 'février', 'mars', 'avril','mai','juin','juille
                     'septembre','octobre','novembre', 'décembre'];
 class ExpertCard extends React.Component {
   state = {
+    pageSize: 50,
     activeTab: "1",
-    prestation: []
+    prestation: null,
+    defaultColDef: {
+      editable: true,
+      sortable: true,
+      resizable: true,
+      filter: true,
+      flex: 1,
+      minWidth: 100,
+    },
+    searchVal: "",
+    columnDefs: [
+      {
+        headerName: "Nom",
+        field: "name",
+        filter: true,
+        width: 250,
+      },
+      {
+        headerName: "Role",
+        field: "role",
+        filter: true,
+        width: 100,
+      },
+        {
+          headerName: "En Attente",
+          field: "role",
+          filter: true,
+          width: 250,
+        },
+        {
+          headerName: "En Cours",
+          field: "role",
+          filter: true,
+          width: 250,
+        },
+        {
+          headerName: "Termine",
+          field: "role",
+          filter: true,
+          width: 250,
+        },
+      
+    ]
   }
 
   toggle = tab => {
@@ -36,10 +86,24 @@ class ExpertCard extends React.Component {
     }
   }
 
+  onGridReady = params => {
+    this.gridApi = params.api
+    this.gridColumnApi = params.columnApi
+    this.gridApi.setDomLayout("autoHeight");
+  }
+  filterSize = val => {
+    if (this.gridApi) {
+      this.gridApi.paginationSetPageSize(Number(val))
+      this.setState({
+        pageSize: val
+      })
+    }
+  }
+
   async componentDidMount() {
     await axios.get(global.config.server_url + "/getMembersPrestation", Config).then(response => {
       this.setState({
-        prestation: response.data.prestation,
+        prestation: response.data,
       })
     })
   }
@@ -47,12 +111,18 @@ class ExpertCard extends React.Component {
   getMembersPrestation(year) {
     axios.get(global.config.server_url + "/getMembersPrestation?year="+year, Config).then(response => {
       this.setState({
-        prestation: response.data.prestation,
+        prestation: response.data,
       })
     })
   }
-
+  updateSearchQuery = val => {
+    this.gridApi.setQuickFilter(val)
+    this.setState({
+      searchVal: val
+    })
+  }
   render() {
+    const { prestation, columnDefs, defaultColDef, pageSize } = this.state
     return (
         <Card>
           <CardHeader>
@@ -122,53 +192,151 @@ class ExpertCard extends React.Component {
           >
                   <TabContent activeTab={this.state.activeTab}>
                     <TabPane tabId="1">
-                    <div className="title-section" style={{textAlign:'center',marginLeft:'auto',marginRight:'auto',marginTop:'10px' ,display:'inline-block',}}>
-                       <div style={{display:'inline-block'}}>
-                       <Input type="select" name="select" id="role" defaultValue={FrenchMonth[new Date().getMonth()]} style={{width:'120px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
-                              onChange={console.log('change')}>
-                             <option>janvier</option><option>février</option><option>mars</option>
-                             <option>avril</option><option>mai</option><option>juin</option>
-                             <option>juillet</option><option>août</option><option>septembre</option>
-                             <option>octobre</option><option>novembre</option><option>décembre</option>
-                       </Input>
-                       </div>
-                       <div style={{display:'inline-block', marginLeft:'5px'}}>
-                       <Input type="select" name="select" id="role" defaultValue={new Date().getFullYear()} style={{width:'75px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
-                              onChange={e => this.getMembersPrestation(e.target.value)}>   
-                             <option>2018</option><option>2019</option><option>2020</option>
-                             <option>2021</option><option>2022</option><option>2023</option>
-                             <option>2024</option><option>2025</option><option>2026</option>
-                             <option>2027</option><option>2028</option><option>2029</option><option>2030</option>
-                       </Input>
-                       </div>
-                       </div>
-                      <p>text Mois.</p>                   
+                       <Row>
+                       <Col sm="12">
+                          <Card style={{minHeight:'500px', minWidth: '1500px'}}>
+                            <CardBody>
+                              <div className="ag-theme-material ag-grid-table">
+                                <div className="ag-grid-actions d-flex justify-content-between flex-wrap mb-1">
+                                <div className="title-section" style={{textAlign:'center',marginRight:'auto' ,display:'inline-block',}}>
+                                <div style={{display:'inline-block'}}>
+                                  <Input type="select" name="select" id="role" defaultValue={FrenchMonth[new Date().getMonth()]} style={{width:'120px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
+                                          onChange={console.log('change')}>
+                                        <option>janvier</option><option>février</option><option>mars</option>
+                                        <option>avril</option><option>mai</option><option>juin</option>
+                                        <option>juillet</option><option>août</option><option>septembre</option>
+                                        <option>octobre</option><option>novembre</option><option>décembre</option>
+                                  </Input>
+                                  </div>
+                                  <div style={{display:'inline-block', marginLeft:'5px'}}>
+                                    <Input type="select" name="select" id="role" defaultValue={new Date().getFullYear()} style={{width:'75px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
+                                          onChange={e => this.getMembersPrestation(e.target.value)}>   
+                                        <option>2018</option><option>2019</option><option>2020</option>
+                                        <option>2021</option><option>2022</option><option>2023</option>
+                                        <option>2024</option><option>2025</option><option>2026</option>
+                                        <option>2027</option><option>2028</option><option>2029</option><option>2030</option>
+                                    </Input>
+                                  </div>
+                                </div>
+                                </div>
+                                {this.state.prestation !== null ? (
+                                  <ContextLayout.Consumer>
+                                    {context => (
+                                      <AgGridReact
+                                        height={'autoHeight'}
+                                        defaultColDef={defaultColDef}
+                                        columnDefs={columnDefs}
+                                        rowData={prestation}
+                                        colResizeDefault={"shift"}
+                                        animateRows={true}
+                                        floatingFilter={true}
+                                        pagination={true}
+                                        pivotPanelShow="always"
+                                        enableRangeSelection={false}
+                                      />
+                                    )}
+                                  </ContextLayout.Consumer>
+                                ) : null}
+                              </div>
+                            </CardBody>
+                          </Card>
+                        </Col>
+                      </Row>                  
                        </TabPane>
                     <TabPane tabId="2">
-                    <div className="title-section" style={{textAlign:'center',marginLeft:'auto',marginRight:'auto',marginTop:'10px' ,display:'inline-block',}}>
-                       <div style={{display:'inline-block', marginLeft:'5px'}}>
-                       <Input type="select" name="select" id="role" defaultValue={new Date().getFullYear()} style={{width:'130px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
-                              onChange={console.log('change')}>   
-                             <option>Trimestre 1</option><option>Trimestre 2</option><option>Trimestre 3</option>
-                             <option>Trimestre 4</option>
-                       </Input>
-                       </div>
-                       </div>
-                      <p>text Trimestre</p>
+                      <Row>
+                       <Col sm="12">
+                          <Card style={{minHeight:'500px', minWidth: '1500px'}}>
+                            <CardBody>
+                              <div className="ag-theme-material ag-grid-table">
+                                <div className="ag-grid-actions d-flex justify-content-between flex-wrap mb-1">
+                                <div className="title-section" style={{textAlign:'center',marginRight:'auto' ,display:'inline-block',}}>
+                                <div style={{display:'inline-block', marginLeft:'5px'}}>
+                                    <Input type="select" name="select" id="role" defaultValue={new Date().getFullYear()} style={{width:'130px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
+                                      onChange={console.log('change')}>   
+                                        <option>Trimestre 1</option><option>Trimestre 2</option><option>Trimestre 3</option>
+                                        <option>Trimestre 4</option>
+                                    </Input>
+                                  </div>
+                                  <div style={{display:'inline-block', marginLeft:'5px'}}>
+                                    <Input type="select" name="select" id="role" defaultValue={new Date().getFullYear()} style={{width:'75px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
+                                          onChange={e => this.getMembersPrestation(e.target.value)}>   
+                                        <option>2018</option><option>2019</option><option>2020</option>
+                                        <option>2021</option><option>2022</option><option>2023</option>
+                                        <option>2024</option><option>2025</option><option>2026</option>
+                                        <option>2027</option><option>2028</option><option>2029</option><option>2030</option>
+                                    </Input>
+                                  </div>
+                                </div>
+                                </div>
+                                {this.state.prestation !== null ? (
+                                  <ContextLayout.Consumer>
+                                    {context => (
+                                      <AgGridReact
+                                        
+                                        height={'autoHeight'}
+                                        defaultColDef={defaultColDef}
+                                        columnDefs={columnDefs}
+                                        rowData={prestation}
+                                        colResizeDefault={"shift"}
+                                        animateRows={true}
+                                        floatingFilter={true}
+                                        pagination={true}
+                                        pivotPanelShow="always"
+                                        enableRangeSelection={false}
+                                      />
+                                    )}
+                                  </ContextLayout.Consumer>
+                                ) : null}
+                              </div>
+                            </CardBody>
+                          </Card>
+                        </Col>
+                      </Row>
                     </TabPane>
                     <TabPane tabId="3">
-                    <div className="title-section" style={{textAlign:'center',marginLeft:'auto',marginRight:'auto',marginTop:'10px' ,display:'inline-block',}}>
-                       <div style={{display:'inline-block', marginLeft:'5px'}}>
-                       <Input type="select" name="select" id="role" defaultValue={new Date().getFullYear()} style={{width:'75px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
-                              onChange={e => this.getMembersPrestation(e.target.value)}>   
-                             <option>2018</option><option>2019</option><option>2020</option>
-                             <option>2021</option><option>2022</option><option>2023</option>
-                             <option>2024</option><option>2025</option><option>2026</option>
-                             <option>2027</option><option>2028</option><option>2029</option><option>2030</option>
-                       </Input>
-                       </div>
-                       </div>
-                      <p> text Annes</p>
+                       <Row>
+                       <Col sm="12">
+                          <Card style={{minHeight:'500px', minWidth: '1500px'}}>
+                            <CardBody>
+                              <div className="ag-theme-material ag-grid-table">
+                                <div className="ag-grid-actions d-flex justify-content-between flex-wrap mb-1">
+                                <div className="title-section" style={{textAlign:'center',marginRight:'auto' ,display:'inline-block',}}>
+                                  <div style={{display:'inline-block', marginLeft:'5px'}}>
+                                    <Input type="select" name="select" id="role" defaultValue={new Date().getFullYear()} style={{width:'75px',marginLeft:'auto',marginRight:'auto',fontSize:'17px'}}
+                                          onChange={e => this.getMembersPrestation(e.target.value)}>   
+                                        <option>2018</option><option>2019</option><option>2020</option>
+                                        <option>2021</option><option>2022</option><option>2023</option>
+                                        <option>2024</option><option>2025</option><option>2026</option>
+                                        <option>2027</option><option>2028</option><option>2029</option><option>2030</option>
+                                    </Input>
+                                  </div>
+                                </div>
+                                </div>
+                                {this.state.prestation !== null ? (
+                                  <ContextLayout.Consumer>
+                                    {context => (
+                                      <AgGridReact
+                
+                                        height={'autoHeight'}
+                                        defaultColDef={defaultColDef}
+                                        columnDefs={columnDefs}
+                                        rowData={prestation}
+                                        colResizeDefault={"shift"}
+                                        animateRows={true}
+                                        floatingFilter={true}
+                                        pagination={true}
+                                        pivotPanelShow="always"
+                                        enableRangeSelection={false}
+                                      />
+                                    )}
+                                  </ContextLayout.Consumer>
+                                ) : null}
+                              </div>
+                            </CardBody>
+                          </Card>
+                        </Col>
+                      </Row>
                     </TabPane>
                   </TabContent>
           </CardBody>
