139ae48d64dd0142b8738b204363ffee996efe83
enzo <enzo.nivert@epitech.eu>
2022-01-11 15:35:22 +0100

    correct some norme issue

--- src/views/apps/dashboard/index.js
+++ src/views/apps/dashboard/index.js
@@ -4,13 +4,10 @@ import React from "react"
 import { Row, Col } from "reactstrap"
 import OverallCard from "./OverallData"
 import ExpertCard from "./ExpertCard"
-import AcompteCard from "./AcompteCard"
-import SoldeCard from "./SoldeCard"
-import YearRevenue from "./YearRevenue"
+
 import RevenueGraph from "./RevenueGraph"
 import PrestationStatistics from "./Prestations"
 import "../../../assets/scss/plugins/charts/apex-charts.scss"
-import axios from "axios";
 
 let $primary = "#7367F0",
   $success = "#28C76F",
@@ -25,7 +22,7 @@ let $primary = "#7367F0",
 class Index extends React.Component {
   state = {
     clients_count: 0,
-    clients_count_list:[],
+    clients_count_list: [],
     current_total_count: 0,
     current_total_amount: 0,
     total_ended_count: 0,
@@ -35,7 +32,7 @@ class Index extends React.Component {
     current_solde_count: 0,
     current_solde_amount: 0,
     opportunite_count: 0,
-    opportunite_amount:0,
+    opportunite_amount: 0,
   }
   // async componentDidMount() {
   //   const Config = {
@@ -65,31 +62,31 @@ class Index extends React.Component {
     return (
       <React.Fragment>
         <Row className="match-height">
-        <Col lg="12" md="6" sm="6">
+          <Col lg="12" md="6" sm="6">
             <OverallCard
-                clients_count={this.state.clients_count}
-                clients_count_list={this.state.clients_count_list}
-                primary={$primary}
-                dangerLight={$danger_light}
-                strokeColor={$stroke_color}
-                labelColor={$label_color}
+              clients_count={this.state.clients_count}
+              clients_count_list={this.state.clients_count_list}
+              primary={$primary}
+              dangerLight={$danger_light}
+              strokeColor={$stroke_color}
+              labelColor={$label_color}
             />
           </Col>
           <Col lg="8" md="6" sm="6">
-          <RevenueGraph
-                strokeColor={$stroke_color}
-                primary={$primary}
-                danger={$danger}
-                labelColor={$label_color}
+            <RevenueGraph
+              strokeColor={$stroke_color}
+              primary={$primary}
+              danger={$danger}
+              labelColor={$label_color}
             />
-            
+
           </Col>
           <Col lg="4" md="6" sm="6">
-          <PrestationStatistics
-                primary={$primary}
-                dangerLight={$danger_light}
-                strokeColor={$stroke_color}
-                labelColor={$label_color}
+            <PrestationStatistics
+              primary={$primary}
+              dangerLight={$danger_light}
+              strokeColor={$stroke_color}
+              labelColor={$label_color}
             />
           </Col>
           <Col>
@@ -97,13 +94,13 @@ class Index extends React.Component {
         </Row>
         <Row className="match-height">
           <Col lg="12" md="6" sm="12">
-          <ExpertCard
-                clients_count={this.state.clients_count}
-                clients_count_list={this.state.clients_count_list}
-                primary={$primary}
-                dangerLight={$danger_light}
-                strokeColor={$stroke_color}
-                labelColor={$label_color}
+            <ExpertCard
+              clients_count={this.state.clients_count}
+              clients_count_list={this.state.clients_count_list}
+              primary={$primary}
+              dangerLight={$danger_light}
+              strokeColor={$stroke_color}
+              labelColor={$label_color}
             />
           </Col>
         </Row>
--- src/views/apps/user/edit/MemberEdit.js
+++ src/views/apps/user/edit/MemberEdit.js
@@ -11,7 +11,7 @@ import {
   TabPane
 } from "reactstrap"
 import classnames from "classnames"
-import {User, Info, Folder} from "react-feather"
+import { User, Info, Folder } from "react-feather"
 import AccountTab from "./Informations"
 import NotesTab from "./Notes"
 import "../../../../assets/scss/pages/users.scss"
@@ -19,12 +19,12 @@ import axios from "axios";
 import Contracts from "./Contracts";
 import Documents from "./Documents";
 //import Task from "./memberTask/Task";
-import {history} from "../../../../history";
+import { history } from "../../../../history";
 import CommentsTab from "./Comments";
 class UserEdit extends React.Component {
   state = {
     rowData: [],
-    persoData:[],
+    persoData: [],
     activeTab: "1"
   }
 
@@ -35,7 +35,7 @@ class UserEdit extends React.Component {
       }
     }
 
-    this.setState({ activeTab: this.props.match.params.tab});
+    this.setState({ activeTab: this.props.match.params.tab });
     await axios.get(global.config.server_url + "/users/" + this.props.match.params.id, Config).then(response => {
       let rowData = response.data
       let persoData = response.data.personal_informations;
@@ -93,12 +93,12 @@ class UserEdit extends React.Component {
                 </NavItem>
                 <NavItem>
                   <NavLink
-                      className={classnames({
-                        active: this.state.activeTab === "3"
-                      })}
-                      onClick={() => {
-                        this.toggle("3")
-                      }}
+                    className={classnames({
+                      active: this.state.activeTab === "3"
+                    })}
+                    onClick={() => {
+                      this.toggle("3")
+                    }}
                   >
                     <Folder size={16} />
                     <span className="align-middle ml-50">Contrats</span>
@@ -106,12 +106,12 @@ class UserEdit extends React.Component {
                 </NavItem>
                 <NavItem>
                   <NavLink
-                      className={classnames({
-                        active: this.state.activeTab === "4"
-                      })}
-                      onClick={() => {
-                        this.toggle("4")
-                      }}
+                    className={classnames({
+                      active: this.state.activeTab === "4"
+                    })}
+                    onClick={() => {
+                      this.toggle("4")
+                    }}
                   >
                     <Folder size={16} />
                     <span className="align-middle ml-50">Documents</span>
@@ -119,12 +119,12 @@ class UserEdit extends React.Component {
                 </NavItem>
                 <NavItem>
                   <NavLink
-                      className={classnames({
-                        active: this.state.activeTab === "5"
-                      })}
-                      onClick={() => {
-                        history.push('/app/member/memberTask/'+this.props.match.params.id+'/all');
-                      }}
+                    className={classnames({
+                      active: this.state.activeTab === "5"
+                    })}
+                    onClick={() => {
+                      history.push('/app/member/memberTask/' + this.props.match.params.id + '/all');
+                    }}
                   >
                     <Folder size={16} />
                     <span className="align-middle ml-50">Tasks</span>
@@ -132,12 +132,12 @@ class UserEdit extends React.Component {
                 </NavItem>
                 <NavItem>
                   <NavLink
-                      className={classnames({
-                        active: this.state.activeTab === "6"
-                      })}
-                      onClick={() => {
-                        this.toggle("6")
-                      }}
+                    className={classnames({
+                      active: this.state.activeTab === "6"
+                    })}
+                    onClick={() => {
+                      this.toggle("6")
+                    }}
                   >
                     <Info size={16} />
                     <span className="align-middle ml-50">Commentaires</span>
@@ -147,36 +147,36 @@ class UserEdit extends React.Component {
               <TabContent activeTab={this.state.activeTab}>
                 <TabPane tabId="1">
                   <AccountTab
-                      data={this.state.rowData}
-                      perso={this.state.persoData}
-                      id={this.props.match.params.id}
-                      dob={this.state.persoData["birth_date"]}
+                    data={this.state.rowData}
+                    perso={this.state.persoData}
+                    id={this.props.match.params.id}
+                    dob={this.state.persoData["birth_date"]}
                   />
                 </TabPane>
                 <TabPane tabId="2">
                   <NotesTab
-                      data={this.state.rowData}
-                      perso={this.state.persoData}
-                      id={this.props.match.params.id}
+                    data={this.state.rowData}
+                    perso={this.state.persoData}
+                    id={this.props.match.params.id}
                   />
                 </TabPane>
                 <TabPane tabId="3">
                   <Contracts
-                      name={this.state.rowData.name}
-                      id={this.props.match.params.id}
+                    name={this.state.rowData.name}
+                    id={this.props.match.params.id}
                   />
                 </TabPane>
                 <TabPane tabId="4">
                   <Documents
-                      name={this.state.rowData.name}
-                      id={this.props.match.params.id}
+                    name={this.state.rowData.name}
+                    id={this.props.match.params.id}
                   />
                 </TabPane>
                 <TabPane tabId="6">
                   <CommentsTab
-                      data={this.state.rowData}
-                      perso={this.state.persoData}
-                      id={this.props.match.params.id}
+                    data={this.state.rowData}
+                    perso={this.state.persoData}
+                    id={this.props.match.params.id}
                   />
                 </TabPane>
               </TabContent>
