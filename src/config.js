da72c602a14ea80059c6e01649ba0efa92107c84
Raphaël Chanliongco <raph.chanliongco@outlook.com>
2025-10-02 14:15:46 +0200

    fix: the config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = global.config = {
-  //server_url: "http://localhost:8000/api",
-  //server_url: "http://vps-a1b847f6.vps.ovh.net:8080/api"
+//   server_url: "http://localhost:8000/api",
+//   server_url: "http://vps-a1b847f6.vps.ovh.net:8080/api"
     server_url: "https://api.optionretraite.net/api"
-  //other global config variables you wish
+//   other global config variables you wish
 };
